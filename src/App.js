2b26515a3ef0275f6c77da5ae4b1fb61a4ce9c06
Austin Tepper <atepper@ameritruck.com>
2024-03-30 09:53:17 -0400

    Remove excel upload functionality for future reference

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 // src/App.js
-import React, { useRef } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom-styles.css';
-import Dashboard from './components/Dashboard';
+import TruckList from './components/TruckList'; // Make sure to import TruckList
 import TruckDetails from './components/TruckDetails';
 import TruckForm from './components/TruckForm';
 import AddMultipleTrucksForm from './components/AddMultipleTrucksForm';
-import * as XLSX from 'xlsx';
-
-
 
 function App() {
-  const fileInputRef = useRef(null);
-
-  const handleExcelUpload = (e) => {
-    const file = e.target.files[0];
-    if (!file) return;
-
-    // Excel file processing logic here
-    // For example:
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      // Process Excel file
-      console.log('Excel file processed');
-    };
-    reader.readAsArrayBuffer(file);
-  };
-
-  const handleUploadClick = () => {
-    fileInputRef.current.click();
-  };
-
   return (
     <Router>
       <div className="container mt-3">
@@ -41,13 +18,11 @@ function App() {
             <div className="navbar-nav">
               <Link className="nav-link" to="/add-truck">Add Truck</Link>
               <Link className="nav-link" to="/add-multiple-trucks">Add Multiple Trucks</Link>
-              <a className="nav-link" href="#" onClick={handleUploadClick}>Excel Upload</a>
-              <input type="file" ref={fileInputRef} onChange={handleExcelUpload} style={{ display: 'none' }} accept=".xlsx, .xls" />
             </div>
           </div>
         </nav>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<TruckList />} /> {/* Add this line for TruckList */}
           <Route path="/add-truck" element={<TruckForm />} />
           <Route path="/truck/:id" element={<TruckDetails />} />
           <Route path="/add-multiple-trucks" element={<AddMultipleTrucksForm />} />
